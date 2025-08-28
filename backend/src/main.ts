d855d9bb985701944230039c564b6c95f6161f52
David Uri <david.uri@byborgenterprises.com>
2025-08-28 14:31:07 +0200

    create express adapter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,21 @@
+import { Express } from "./adapters/express/Express";
 import { Socket } from "./adapters/socket/Socket";
 import { createChatModule } from "./modules/chat";
 import { ChatSocket } from "./modules/chat/ChatSocket";
 import { createMessageModule } from "./modules/message";
 import { createUserModule } from "./modules/user";
 
-const { server } = new Socket();
+const PORT = parseInt(process.env.PORT || "3000", 10) || 3000;
+
+const express = new Express();
+const httpServer = express.create();
+const { server } = new Socket({ httpServer });
 const { userService } = createUserModule();
 const { messageService } = createMessageModule();
 const { chatService } = createChatModule({ userService, messageService });
 
 new ChatSocket({ server, userService, chatService });
+
+httpServer.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
